75ec44c7c95e4d0ce5c268b8b39e270557d08347
Dustin Frost <s2kjunky@yahoo.com>
2018-02-19 19:55:03 -0600

    add more notes to script

--- Udemymachlearn1.R
+++ Udemymachlearn1.R
@@ -40,3 +40,67 @@ ggplot()+geom_line(data=df, aes(x=x,y=y))
 
 
 #################################################################################################
+sample(1:100, 1, replace = TRUE)
+rnorm(100, 50, 10)
+
+x <- rep(1,100)
+y <- rnorm(100, 50, 10)
+df <- data.frame(x,y)
+df
+mean <- data.frame(x,y)
+x <- 1
+y <- 50
+
+ggplot()+geom_point(data=df, aes(x=x, y=y))+
+geom_point(data=mean, aes(x=x, y=y), size=2, color="red")
+
+
+#################################################################################################
+library(ggplot2)
+library(Hmisc)
+x <- c(rep(1,100),rep(9,100), rep(15,100))
+y <- c(rnorm(100,50,10), rnorm(100,30,10), rnorm(100,78,10))
+df <- data.frame(x,y)
+
+x <- c(1,9,15)
+y <- c(50,30,78)
+means <- data.frame(x,y)
+
+ggplot()+
+  geom_point(data=df, aes(x=x, y=y))+
+  geom_point(data=means, aes(x=x, y=y), size=4, color="red")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
