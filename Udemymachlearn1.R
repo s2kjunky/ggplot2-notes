e62c6ea5ce5f32beb1b6b66b0cb453c23b74c8dc
Dustin Frost <s2kjunky@yahoo.com>
2018-02-25 19:53:06 -0600

    add r script update and plot image

--- Udemymachlearn1.R
+++ Udemymachlearn1.R
@@ -101,20 +101,42 @@ ggplot()+
   geom_point(data=dat, aes(x=x, y=y))
 
 #################################################################################################
+library(ggplot2)
+x <- rnorm(100,10,5)
+y<-3*x+1
+means<-data.frame(x,y)
+x<-c(-5, 25)
+y<-3*x+1
+line<- data.frame(x,y)
+x <- means$x
+y <- sapply(x, function(x) rnorm(1,3*x+1,10))
+dat <- data.frame(x,y)  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ggplot()+geom_point(data=means, aes(x=x, y=y),color="red")+
+geom_line(data = line,aes(x=x, y=y))+
+scale_x_continuous(limits = c(-10,30))+
+scale_y_continuous(limits = c(-20,80))+
+geom_point(data = dat, aes(x=x, y=y))
 
 #################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
